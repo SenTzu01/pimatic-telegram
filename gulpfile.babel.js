594300b574994e5cda538c9a95e38581ee6c6b93
SenTzu01 <sentzu01@gmail.com>
2017-01-08 17:57:30 +0100

    added gulpfile

---
+++ gulpfile.babel.js
@@ -0,0 +1,10 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+ 
+gulp.task('default', () => {
+    return gulp.src('./node_modules/telebot/lib/*.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('./node_modules/telebot/dist'));
+});
